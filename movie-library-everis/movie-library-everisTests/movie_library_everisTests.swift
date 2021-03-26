694a39d1648887bd91006d674300aaf80480c7ec
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-26 13:58:40 -0300

    :tada: initial commit

---
+++ movie-library-everis/movie-library-everisTests/movie_library_everisTests.swift
@@ -0,0 +1,33 @@
+//
+//  movie_library_everisTests.swift
+//  movie-library-everisTests
+//
+//  Created by Elisa Camillo on 26/03/21.
+//
+
+import XCTest
+@testable import movie_library_everis
+
+class movie_library_everisTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
