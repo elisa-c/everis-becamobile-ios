f1b53b7db0d33841b1d6ee95edb606c86b2b4581
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-27 22:25:51 -0300

    :white_check_mark: adding first test - checking if data requested from api is retrieved

---
+++ MovieLibrary/MovieLibrary/Model/MoviesAPI.swift
@@ -0,0 +1,25 @@
+//
+//  MoviesAPI.swift
+//  MovieLibrary
+//
+//  Created by Elisa Camillo on 27/03/21.
+//
+
+import UIKit
+import Alamofire
+
+class MoviesAPI: NSObject {
+    
+    func retrieveTrendingMovies() {
+        
+        let api_key: String = "a3627ac6d1cea57a39a026d0d83a6a0a"
+        
+        guard let url = URL(string: "https://api.themoviedb.org/3/trending/all/week?api_key=\(api_key)&language=pt-BR")
+        else {return}
+        
+        AF.request(url, method: .get).responseJSON { (data) in
+            print(data)
+          }
+        
+    }
+}
